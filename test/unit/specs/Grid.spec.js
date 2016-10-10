9b9d514f8bdc145f34458f5a9b52fea8ad75345e
Matt Szabo <szabo.matthew@gmail.com>
2016-10-11 10:42:28 +1100

    testing for button count

--- test/unit/specs/Grid.spec.js
+++ test/unit/specs/Grid.spec.js
@@ -3,12 +3,17 @@ import Grid from 'src/components/Grid'
 import store from 'src/store/store'
 
 describe('Grid.vue', () => {
-  it('should render correct contents', () => {
-    const vm = new Vue({
-      el: document.createElement('div'),
-      store,
-      render: (h) => h(Grid)
-    })
+  const vm = new Vue({
+    el: document.createElement('div'),
+    store,
+    render: (h) => h(Grid)
+  })
+
+  it('renders correct header contents', () => {
     expect(vm.$el.querySelector('#my-grid h1').textContent).to.equal('Grid app')
+  })
+
+  it('renders 4 buttons', () => {
+    expect(vm.$el.querySelectorAll('button').length).to.equal(4)
   })
 })
