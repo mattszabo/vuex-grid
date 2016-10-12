2e3bdc45e744f754e67a9927f5cf181c784a5cef
Matt Szabo <szabo.matthew@gmail.com>
2016-10-12 15:01:18 +1100

    use switch block for movement

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,7 +2,6 @@ export const isSameSquare = function(pos1, pos2) {
   return (pos1.row === pos2.row && pos1.col === pos2.col)
 }
 
-// only supports one position, lets work in multiple positions
 export const findRandomEmptySquare = function(position, width = 10, height = 10) {
   let uniquePos = false
   let row, col
@@ -18,14 +17,21 @@ export const findRandomEmptySquare = function(position, width = 10, height = 10)
 
 export const mutations = {
   MOVE(state, direction) {
-    if (direction === 'UP') {
-      state.playerPos.row < 1 ? state.playerPos.row : state.playerPos.row--
-    } else if (direction === 'DOWN') {
-      state.playerPos.row >= state.height - 1 ? state.playerPos.row : state.playerPos.row++
-    } else if (direction === 'LEFT') {
-      state.playerPos.col < 1 ? state.playerPos.col : state.playerPos.col--
-    } else if (direction === 'RIGHT') {
-      state.playerPos.col >= state.width - 1 ? state.playerPos.col : state.playerPos.col++
+    switch (direction) {
+      case 'UP':
+        state.playerPos.row < 1 ? state.playerPos.row : state.playerPos.row--
+        break
+      case 'DOWN':
+        state.playerPos.row >= state.height - 1 ? state.playerPos.row : state.playerPos.row++
+        break
+      case 'LEFT':
+        state.playerPos.col < 1 ? state.playerPos.col : state.playerPos.col--
+        break
+      case 'RIGHT':
+        state.playerPos.col >= state.width - 1 ? state.playerPos.col : state.playerPos.col++
+        break
+      default:
+        break
     }
     // apply game logic
     let { playerPos, foodPos } = state
